e5febe75d6c3dacbb481eafe642a606ac06b30f6
Boranbektas <boranbek1234@gmail.com>
2022-12-15 23:30:43 +0300

    boran commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 #include "class.h";
+
+
 void person::print(void){
     cout<<"Ali boran buraya yazdı"<<endl;
    
@@ -8,6 +10,8 @@ void person::print(void){
 }
 
 int main(void){
-    
+    person p1;
+    cout<<"Ali boran xd"<<endl;
+    p1.print();
 
 }
