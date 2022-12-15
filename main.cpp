71cbe2b6ce2dfe3f14e0bccc5b12dff0b4bbdff1
Boranbektas <boranbek1234@gmail.com>
2022-12-15 22:48:30 +0300

    boran second comit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
+#include "class.h"
 
 int main(void){
 
     cout << "deneme";
     cout << "mrb";
     cout<<"ANANIN AMI DONUNDA"<<endl;
-    
+
+}
+void person::print(void){
+    cout<<"Ali boran buraya yazdı"<<endl;
 }
