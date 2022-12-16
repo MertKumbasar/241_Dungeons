3624bf0245fe6f9aa2c5358bdae859254b9f524d
Boranbektas <boranbek1234@gmail.com>
2022-12-16 23:41:11 +0300

    Boran last Commit Get/set

--- Player.h
+++ Player.h
@@ -1,6 +1,35 @@
-class player{
+#ifndef Player_H
+#define Player_H
 
+
+class Player{
+    
+
+    protected:
+        string Name;
+        int Health=0;
+        int Mana;
+        int Level;
+        int Xp;
+        int Attack;
+        int Defense;
+    public:
+    //Default Constructor//
+    
+    //Overloaded Constructors//
+    
+
+    //Destructor//
+    
+    //Methods//
+    void Setname(string input){Name = input;}
+    string GetName(void){return Name;}
+    void SetHealth(int input){Health=input;}
+    int GetHealth(void){return Health;}
+    
 
 
 
 };
+
+#endif
