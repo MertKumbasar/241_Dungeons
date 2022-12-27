48162abd56dd7e4878bb78d97be78aa2b1d3a912
Hazal <113294096+hazalkntr@users.noreply.github.com>
2022-12-27 20:06:47 +0300

    haha

--- main.cpp
+++ main.cpp
@@ -20,18 +20,20 @@ void gotoxy(int x, int y) {
 int main(){
       string name;
       int answer;
-      cout << "The voice: Welcome stranger what is your name ? "<< endl;
+      cout << "The voice: Welcome stranger, what is your name ? "<< endl;
       cin >> name;
+      cout << endl;
       cout << "Well met " << name << ". Here is the deal " << name << ", you were camping with your girlfriend in the forest but somehow you end up here and your girlfirend is missing" << endl;
       
       cout << "'ask a question' "<< endl;
       do{
-            cout << "1. Wait a minute who are you ? "<< endl;
-            cout << "2. Where is may girlfriend ? Tell me NOW !!!! " << endl;
-            cout << "3.Explore the dungeon !!"<<endl;
+            cout << "1. Wait a minute, who are you ? "<< endl;
+            cout << "2. Where is my girlfriend ? Tell me NOW !!!! " << endl;
+            cout << "3. Explore the dungeon !!"<<endl;
             cin >> answer;
+            cout << endl;
             if(answer == 1){
-                  cout << "The voice: It realy dosent matter who i am i am just here to help you"<<endl;
+                  cout << "The voice: It really doesn't matter who i am. I am just here to help you"<<endl;
             }
             else if(answer == 2){
                   cout << "The voice: Thats a good question my friend but i think the answer is deep down in the dungeon"<<endl;
