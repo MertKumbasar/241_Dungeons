352a5268f279ef13b330a3c502419edee3a55622
MertKumbasar <kumbasarmert@gmail.com>
2023-01-02 20:34:47 +0300

    push

---
+++ ASCII_arts.h
@@ -0,0 +1,45 @@
+
+#ifndef ASCII_arts_H
+#define ASCII_arts_H
+
+#include <iostream>
+using namespace std;
+
+
+void printTitle(void){
+      cout << "             _ " << endl;
+      cout << "            | |" << endl;
+      cout << "          __| |_   _ _ __   __ _  ___  ___  _ __  ___ " << endl;
+      cout << "         / _` | | | | '_ \\ / _` |/ _ \\/ _ \\| '_ \\/ __|" << endl;
+      cout << "        | (_| | |_| | | | | (_| |  __/ (_) | | | \\__ \\" << endl;
+      cout << "         \\__,_|\\__,_|_| |_|\\__, |\\___|\\___/|_| |_|___/" << endl;
+      cout << "                            __/ |" << endl;
+      cout << "                           |___/" << endl;
+      cout << "   _________________________________________________________" << endl;
+      cout << " /|     -_-                                             _-  |\\" << endl;
+      cout << "/ |_-_- _                                         -_- _-   -| \\" << endl;
+      cout << "  |                            _-  _--                      |" << endl;
+      cout << "  |                            ,                            |" << endl;
+      cout << "  |      .-'````````'.        '(`        .-'```````'-.      |" << endl;
+      cout << "  |    .` |           `.      `)'      .` |           `.    |" << endl;
+      cout << "  |   /   |   ()        \\      U      /   |    ()       \\   |" << endl;
+      cout << "  |  |    |    ;         | o   T   o |    |    ;         |  |" << endl;
+      cout << "  |  |    |     ;        |  .  |  .  |    |    ;         |  |" << endl;
+      cout << "  |  |    |     ;        |   . | .   |    |    ;         |  |" << endl;
+      cout << "  |  |    |     ;        |    .|.    |    |    ;         |  |" << endl;
+      cout << "  |  |    |____;_________|     |     |    |____;_________|  |" << endl;
+      cout << "  |  |   /  __ ;   -     |     !     |   /     `'() _ -  |  |" << endl;
+      cout << "  |  |  / __  ()        -|        -  |  /  __--      -   |  |" << endl;
+      cout << "  |  | /        __-- _   |   _- _ -  | /        __--_    |  |" << endl;
+      cout << "  |__|/__________________|___________|/__________________|__|" << endl;
+      cout << " /                                             _ -           \\" << endl;
+      cout << "/   -_- _ -             _- _---                       -_-  -_ \\" << endl;
+      system("pause");
+      system("cls");
+}
+void printWarrior(void){
+    
+}
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "Player.h"
 #include "Enemy.h"
 #include "Item.h"
-
+#include "ASCII_arts.h"
 
 
 //template <typename T > 
@@ -14,38 +14,6 @@
 
 // }
 
-// function to print title
-void printTitle(void){
-      cout << "             _ " << endl;
-      cout << "            | |" << endl;
-      cout << "          __| |_   _ _ __   __ _  ___  ___  _ __  ___ " << endl;
-      cout << "         / _` | | | | '_ \\ / _` |/ _ \\/ _ \\| '_ \\/ __|" << endl;
-      cout << "        | (_| | |_| | | | | (_| |  __/ (_) | | | \\__ \\" << endl;
-      cout << "         \\__,_|\\__,_|_| |_|\\__, |\\___|\\___/|_| |_|___/" << endl;
-      cout << "                            __/ |" << endl;
-      cout << "                           |___/" << endl;
-      cout << "   _________________________________________________________" << endl;
-      cout << " /|     -_-                                             _-  |\\" << endl;
-      cout << "/ |_-_- _                                         -_- _-   -| \\" << endl;
-      cout << "  |                            _-  _--                      |" << endl;
-      cout << "  |                            ,                            |" << endl;
-      cout << "  |      .-'````````'.        '(`        .-'```````'-.      |" << endl;
-      cout << "  |    .` |           `.      `)'      .` |           `.    |" << endl;
-      cout << "  |   /   |   ()        \\      U      /   |    ()       \\   |" << endl;
-      cout << "  |  |    |    ;         | o   T   o |    |    ;         |  |" << endl;
-      cout << "  |  |    |     ;        |  .  |  .  |    |    ;         |  |" << endl;
-      cout << "  |  |    |     ;        |   . | .   |    |    ;         |  |" << endl;
-      cout << "  |  |    |     ;        |    .|.    |    |    ;         |  |" << endl;
-      cout << "  |  |    |____;_________|     |     |    |____;_________|  |" << endl;
-      cout << "  |  |   /  __ ;   -     |     !     |   /     `'() _ -  |  |" << endl;
-      cout << "  |  |  / __  ()        -|        -  |  /  __--      -   |  |" << endl;
-      cout << "  |  | /        __-- _   |   _- _ -  | /        __--_    |  |" << endl;
-      cout << "  |__|/__________________|___________|/__________________|__|" << endl;
-      cout << " /                                             _ -           \\" << endl;
-      cout << "/   -_- _ -             _- _---                       -_-  -_ \\" << endl;
-      system("pause");
-      system("cls");
-}
 
 
 string firstChapter(){
