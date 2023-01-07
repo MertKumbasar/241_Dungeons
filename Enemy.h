c96a301e9f245fcdff65112b38a56221ccec92f1
Jzerq <94188210+Jzerq@users.noreply.github.com>
2023-01-08 01:22:11 +0300

    rockpapaer

--- Enemy.h
+++ Enemy.h
@@ -3,6 +3,10 @@
 #include "Entity.h"
 using namespace std;
 #include <string>
+#include <iostream>
+#include <string>
+#include <map>
+#include <random>
 
 class Enemy : public Entity
 {
@@ -17,5 +21,90 @@ public:
     // destructor
     ~Enemy() {}
 };
+
+class RPC{
+    private:
+      string *Mrpc {nullptr},Computer_Name;
+      int index;
+      int winner1,winner2;
+
+    public:
+      RPC(): index{3}, Computer_Name{"AI"}, winner1 {0}, winner2{0} {
+
+          Mrpc= new string [index];
+          Mrpc[0]="rock";
+          Mrpc[1]="paper";
+          Mrpc[2]="scissors";        
+      }
+      
+      int GetWinner (string x, string y){
+
+        if(x=="rock")
+          {
+            if (y=="rock")
+              return 0;
+            else if (y=="paper")
+              winner1++;
+            else if (y=="scissors")
+              winner2++;
+
+          }
+        else if (x=="paper"){
+
+            if (y=="rock")
+              winner1++;
+            else if (y=="paper")
+              return 0;
+            else if (y=="scissors")
+              winner2++;
+
+        }
+        else if (x=="scissors"){
+
+            if (y=="rock")
+              winner2++;
+            else if (y=="paper")
+              winner1++;
+            else if (y=="scissors")
+              return 0;
+
+        }
+
+          
+      }
+      void CheckResult(){
+        int rnd;
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(1, 3);
+
+        rnd=dis(gen);
+
+        string round,computer;      
+
+        do{
+
+          computer=Mrpc[rnd]; 
+
+          cout<<"Enter Your Choice "<<endl;
+          cin>>round;
+          cout<<Computer_Name<<" Generated "<<computer<<endl;
+
+          GetWinner (round,computer);
+
+        }while (winner1!=3||winner2!=3);
+        
+        if (winner1==3){
+          cout<<"PLAYER WINS"<<endl;
+        }
+        else if(winner2==3)
+          cout<<"Computer wins"<<endl;
+        
+      }
+      
+
+};
+
+
 
 #endif
